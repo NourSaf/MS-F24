993145c14fa1f74a52644637f3e1b036d52ab3eb
Nour Al Safadi <nour.safadi7@icloud.com>
2024-11-20 11:08:11 -0500

    solved the topics issue

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-// const { toArray } = require("@amcharts/amcharts5/.internal/core/util/Array");
-
-// const { object } = require("@amcharts/amcharts5");
 
 
 const data = await d3.json('./myData.json')
@@ -19,11 +16,9 @@ for (let i = 0; i < topic.length; i++){
 }
 console.log(topics)
 
-
 //words processing to get the most frequent topics within the book collection
 let phrase;
-let frequency = [];
-
+let newPhrase = []
 topics.forEach( topic => {
   let split = topic.split(' ')
   let words = split.map(word => word.toLowerCase());
@@ -33,66 +28,100 @@ topics.forEach( topic => {
     if(word.length == 0) return;
     // remove special characters
     phrase = word.replace(/[^a-zA-Z ]/g, " ");
+    // console.log('this is phrase', phrase)
+    newPhrase.push(phrase)
+  })
+});
+console.log('this is my new phrase',newPhrase)
 
-    // check if the word is in the array
+let splited;
+let frequency = [];
+newPhrase.forEach(word => {
+  splited = word.split(' ');
+  console.log('This is splited', splited)
+  splited.forEach(i => {
+    let splited_word = i;
+    if (splited_word.length == 0) return; 
+    // console.log("this is splited words",splited_word)
     let match = false;
-    frequency.forEach(i => {
-      if(i.word == word) {
+    frequency.forEach( i => { 
+      if (i.splited_word == splited_word){
         i.count++;
         match = true;
       }
+      
     });
-    // if not, add it to the array
-    if(!match) {
+    if (!match){
       frequency.push({
-        word: phrase,
-        count: 1
+        splited_word: splited_word,
+        count: 1,
       });
-    };
-  });
-  
-});
-// console.log(frequency);
-  // // sort the frequency map
-const sorted = frequency.sort((a, b) => (a.count < b.count) ? 1 : -1);
-  console.log("this is my words sorted",sorted)
+    }
 
-  const filtered = 
+  })
+})
+const sorted =  frequency.sort((a, b) => (a.count < b.count) ? 1 : -1);
+console.log("this is sorted frequency", sorted)
 
-function analyzeData(lines) {
-  let phrase;
-  let frequency = [];
-  // loop over the array
-  lines.forEach(line => {
-    // split the line into words
-    let words = line.split(' ');
-    // loop over the words
-    words.forEach(word => {
-      // remove empty words
-      if(word.length == 0) return;
-      // remove special characters
-      phrase = word.replace(/[^a-zA-Z ]/g, "");
-      // check if the word is in the array
-      let match = false;
-      frequency.forEach(i => {
-        if(i.word == word) {
-          i.count++;
-          match = true;
-        }
-      });
-      // if not, add it to the array
-      if(!match) {
-        frequency.push({
-          word: phrase,
-          count: 1
-        });
-      }
-    });
-  });
 
-  // show the frequency map
-  console.log(frequency);
-  // sort the frequency map
-  frequency.sort((a, b) => (a.count < b.count) ? 1 : -1);
-  return frequency;
-}
+
+
+// function analyzeData(lines) {
+//   let phrase;
+//   let frequency = [];
+//   // loop over the array
+//   lines.forEach(line => {
+//     // split the line into words
+//     let words = line.split(' ');
+//     // loop over the words
+//     words.forEach(word => {
+//       // remove empty words
+//       if(word.length == 0) return;
+//       // remove special characters
+//       phrase = word.replace(/[^a-zA-Z ]/g, "");
+//       // check if the word is in the array
+//       let match = false;
+//       frequency.forEach(i => {
+//         if(i.word == word) {
+//           i.count++;
+//           match = true;
+//         }
+//       });
+//       // if not, add it to the array
+//       if(!match) {
+//         frequency.push({
+//           word: phrase,
+//           count: 1
+//         });
+//       }
+//     });
+//   });
+
+//   // show the frequency map
+//   console.log(frequency);
+//   // sort the frequency map
+//   frequency.sort((a, b) => (a.count < b.count) ? 1 : -1);
+//   return frequency;
+// }
+
+// check if the word is in the array
+    // let match = false;
+    // frequency.forEach(i => {
+    //   if(i.word == word) {
+    //     i.count++;
+    //     match = true;
+    //   }
+    // });
+
+    // // if not, add it to the array
+    // if(!match) {
+    //   frequency.push({
+    //     word: phrase,
+    //     count: 1
+    //   });
+    // };
+
+
+    // // sort the frequency map
+// const sorted = frequency.sort((a, b) => (a.count < b.count) ? 1 : -1);
+// console.log("this is my words sorted",sorted)
