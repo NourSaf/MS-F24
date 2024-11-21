ca6d75498be6d058d956fe592825ded21d1bfa6c
Nour Al Safadi <nour.safadi7@icloud.com>
2024-11-21 12:16:00 -0500

    adds new don Chart

--- js/main copy 3.js
+++ js/main copy 3.js
@@ -1,5 +1,6 @@
 import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";
 
+
 const data = await d3.json('./myData.json');
 console.log(data);
 
@@ -37,10 +38,8 @@ newPhrase.forEach(word => {
 });
 
 const sorted = frequency.sort((a, b) => b.count - a.count);
-const topic_filtered = sorted.filter(item => !["and", "an","in","of","etc"].includes(item.splited_word) && item.splited_word.length > 2);
-console.log("this count filtered",topic_filtered)
-
-
+const topic_filtered = sorted.filter(item => !["and", "an","in"].includes(item.splited_word) && item.splited_word.length >= 2);
+console.log(topic_filtered)
 
 const categories = {
   "Government & Politics": ["government", "politics", "political", "policy", "state", "presidents", "nationalism", "parties", "elections", "public"],
@@ -54,7 +53,6 @@ const categories = {
   "Religion & Philosophy": ["religion", "religious", "philosophy", "moral", "belief"],
   "Environment": ["environmental", "land"]
 };
-
 
 const topics_category = await d3.json("./topics_3.json");
 const categorizedTopics = {};
@@ -78,8 +76,8 @@ Object.keys(categories).forEach((category, index) => {
 });
 console.log(categoryColors);
 
-
 data.forEach(item => {
+  const name = item.name;
   const topics = item.topic;
 
   let newPhrase = [];
@@ -115,8 +113,6 @@ data.forEach(item => {
       }
     }
   });
-
-
 
 const mainContainer = d3.select(".mainBodyContent")
   .style("display", "flex")
@@ -159,60 +155,6 @@ svg.selectAll("g")
   
 });
 
-const topics_year = d3.rollup(data,
-  (v)=> v.length,
-  (d) => d.bookYear,
-)
-
-// const timelineSvg = d3.select(".timeline")
-//   .append("svg")
-//   .attr("width", 800)
-//   .attr("height", 400);
-
-// const xScale = d3.scaleBand()
-//   .domain(Array.from(topics_year.keys()))
-//   .range([0, 1000])
-//   .padding(0.1);
-
-// const yScale = d3.scaleLinear()
-//   .domain([0, d3.max(Array.from(topics_year.values()))])
-//   .range([400, 0]);
-
-// timelineSvg.append("g")
-//   .attr("transform", "translate(0, 380)")
-//   .call(d3.axisBottom(xScale).tickFormat(d3.format("d")));
-
-// timelineSvg.append("g")
-//   .attr("transform", "translate(20, 0)")
-//   .call(d3.axisLeft(yScale));
-
-// timelineSvg.selectAll(".bar")
-//   .data(Array.from(topics_year.entries()))
-//   .enter()
-//   .append("rect")
-//   .attr("class", "bar")
-//   .attr("x", d => xScale(d[0]))
-//   .attr("y", d => yScale(d[1]))
-//   .attr("width", xScale.bandwidth())
-//   .attr("height", d => 400 - yScale(d[1]))
-//   .attr("fill", "#69b3a2");
-
-console.log("Topic year",topics_year)
-const plotData = Array.from(topics_year.entries()).map(([year, count]) => ({ year, count }));
-
-const plot = Plot.plot({
-  grid: false,
-  inset: 10,
-  color: { legend: false },
-  marks: [
-    // Plot.frame(),
-    Plot.dot(plotData, { x: "year", y: "count", stroke: "count" })
-  ]
-});
-
-const timeline_div = document.querySelector(".timeline");
-timeline_div.appendChild(plot);
-
 const legendDiv = d3.select('.legend')
 const legend = legendDiv.append("div").attr("class", "legend");
 
@@ -252,49 +194,3 @@ const legend = legendDiv.append("div").attr("class", "legend");
 
 // const timeline_div = document.querySelector(".timeline");
 // timeline_div.appendChild(plot)
-
-const donChart_width  = 150;
-const donChart_height = 150;
-const donChart_margin = 40;
-
-// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-let radius = Math.min(donChart_width, donChart_height) / 2 - donChart_margin
-
-// append the svg object to the div called 'my_dataviz'
-let svg = d3.select(".my_dataviz")
-  .append("svg")
-    .attr("width", donChart_width)
-    .attr("height", donChart_height)
-  .append("g")
-    .attr("transform", "translate(" + donChart_width / 2 + "," + donChart_height / 2 + ")");
-
-var testData = {a: 9, b: 20, c:30, d:8, e:12}
-
-// set the color scale
-var color = d3.scaleOrdinal()
-  .domain(Object.keys(categories))
-  .range(["#6e40aa","#bf3caf","#fe4b83","#ff7847","#e2b72f","#aff05b","#52f667","#1ddfa3","#23abd8","#4c6edb","#6e40aa"])
-
-// Compute the position of each group on the pie:
-var pie = d3.pie()
-  .value((d) => d)
-var data_ready = pie(Object.values(testData))
-
-// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-svg
-  .selectAll('whatever')
-  .data(data_ready)
-  .enter()
-  .append('path')
-  .attr('d', d3.arc()
-    .innerRadius(50)         // This is the size of the donut hole
-    .outerRadius(radius)
-  )
-  .attr('fill', function(d, i){ return(color(Object.keys(testData)[i])) })
-  .attr("stroke", "black")
-  .style("stroke-width", "2px")
-  .style("opacity", 0.7)
-
-  const object_cat = Object.values(categories)
-  console.log("this is my object cat",object_cat)
-  
