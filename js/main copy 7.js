3b34cb87fb5aefc42f7938f401e6a0f9ed4b29ff
Nour Al Safadi <nour.safadi7@icloud.com>
2024-11-22 23:37:28 -0500

    cleans code and organize functions

--- js/main copy 7.js
+++ js/main copy 7.js
@@ -1,4 +1,3 @@
-
 import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";
 
 const categories = {
@@ -13,9 +12,13 @@ const categories = {
   "Religion & Philosophy": ["religion", "religious", "philosophy", "moral", "belief"],
   "Environment": ["environmental", "land"]
 };
-
 const BOOK = "name";
-const categoryColors = {};
+const PETAL_LENGTH = "petallength";
+const PETAL_WIDTH =  "petalwidth";
+const SEPAL_LENGTH = "sepallength";
+const SEPAL_WIDTH = "sepalwidth";
+const TOOLTIP_WIDTH =  150;
+const TOOLTIP_HEIGHT =  20;
 
 let state = {
   data: [],
@@ -36,28 +39,16 @@ let xScale, yScale, colorScale;
 
 async function dataLoad() {
   // we can set up our layout before we have data
-  //Set up the layout before the load of data -> call layoutIntialization();
-  initializeLayout()
   const data = await d3.json('./myData.json');
-  console.log("This is my Data",data);
+  console.log(data);
+
+
+  // once data is on state, we can access it from any other function because state is a global variable
   
   // we also populate our checkboxes with values from the data
   const categories_names = Object.keys(categories)
-  console.log("This is Categories name",categories_names)
-  //the same result as from Object.keys we get from Array.from after getting the keys from the object and puting then in an array
-  const checkboxValues = Array.from(new Set(categories_names.map(d => d)))
-  console.log("This is my checkbox Value", checkboxValues)
-  
-  setState({
-    data: data.map((d,i) => ({
-      ...d,
-      id: d[BOOK] + "_" + i,
-    })),
-    filters: {
-      manu: checkboxValues,
-      checked: checkboxValues,
-    }
-  })
+  console.log("This is Categories name",categories)
+  const checkboxValues = Array.from(new Set(categories.map(d => d[CLASS])))
 }
 
 // whenever state changes, update the state variable, then redraw the viz
@@ -65,106 +56,10 @@ function setState(nextState) {
   // console.log("state updated");
   // using Object.assign keeps the state *immutable*
   state = Object.assign({}, state, nextState);
-  console.log("This is my State",state)
   draw();
 }
 
-function onCheckboxChange(e){
 
-  console.log("This is target Name", e.target.name);
-  const index = state.filters.checked.indexOf(d.target.name)
-  const isBoxChecked = index > -1;
-  let nextCheckeValues;
-  if(isBoxChecked){
-    nextCheckeValues = [
-      ...state.filters.checked.slice(0, index),
-      ...state.filters.checked.slice(index + 1)
-    ]
-  }
-  else {
-    nextCheckeValues = [
-      ...state.filters.checked,
-      d.target.name,
-    ]
-  }
-  setState({
-    filters: {
-      ...state.filters,
-      checked: nextCheckeValues,
-    },
-  });
-}
-
-//--------------------------------
-// HERE, you can ADD a MOUSEOVER function for a hover effect to show the details for each book
-//--------------------------------
-
-//This is layout function
-function initializeLayout(){
-  const SVG_ = 800;
-  const SVG_HEIGHT = 800;
-  const MARGIN = 80; 
-
-const parent = d3.select('.mainVizContent')
-
-  const filterMenu = d3.select('.legend')
-  filterMenu.append('div')
-    .attr('class',)
-//legend for the first books and categories
-const legendDiv = d3.select('.legend')
-const legend = legendDiv.append("div").attr("class", "legend");
-
-colorScale = d3.scaleOrdinal(["#6e40aa","#bf3caf","#fe4b83","#ff7847","#e2b72f","#aff05b","#52f667","#1ddfa3","#23abd8","#4c6edb","#6e40aa"]);
-
-Object.keys(categories).forEach((category, index) => {
-  categoryColors[category] = colorScale(index);
-});
-console.log("This is each category color",categoryColors);
-
-
-Object.entries(categoryColors).forEach(([category, color]) => {
-const legendItem = legend.append("div").attr("class", "legend-item")
-  .style("display", "flex")
-  .style("align-items", "center")
-  .style("margin-bottom", "5px");
-
-legendItem.append("div")
-  .style("width", "12px")
-  .style("height", "12px")
-  .style("background-color", color)
-  .style('border-radius','20px')
-  .style("margin-right", "5px");
-
-legendItem.append("span").text(category).attr('class','filters');
-});
-}
-
-function draw(){
-  const filteredData = state.data
-  .filter(d => state.filters.checked.indexOf(d[BOOK]>-1));
-  
-  const checkRow = d3 
-    .select('.filters')
-    .selectAll('.check-row')
-    .data(state.filters.menu)
-    .join('div')
-    .attr('class','check-row')
-    .html(
-      d => `
-      <input name="${d}" type = "checkbox" ${state.filters.checked.indexOf(d)>-1 ? "checked":""}></input>
-      <lable for="${d}"> ${d} </lable>
-      `
-    );
-  checkRow.select("input").on('change',onCheckboxChange);
-
-  //----------------------------------------
-  // Continue here to upade how many bars to show when clicked on a check box
-  //----------------------------------------
-}
-
-
-
-async function dataProcessing(){
 
 const topics_category = await d3.json("./topics_3.json");
 const categorizedTopics = {};
@@ -188,10 +83,10 @@ Object.keys(categories).forEach((category, index) => {
   categoryColors[category] = colorScale(index);
 });
 console.log("This is each category color",categoryColors);
-}
 
-async function draws(){
-const data = await d3.json("./myData.json");
+
+
+function draw(){
 data.slice(0, 2000).forEach(item => {
   const topics = item.topic;
   let newPhrase = [];
@@ -301,4 +196,4 @@ function showContent(){
   });
 }
 
-dataLoad()
+showContent()
